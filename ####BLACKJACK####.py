22c9c15d24f65596dddda0c5b89b3c8fef4b365d
Devin Kenney <Kenneypdevin@gmail.comm>
2023-12-08 17:28:11 -0500

    proved random card generator

--- ####BLACKJACK####.py
+++ ####BLACKJACK####.py
@@ -1,5 +1,15 @@
 
 ####BLACKJACK####
-from random import random
+import random
 
-cards = {"Ace":11, "Two":2, "Three":3, "Four":4, "Five":5, "Six":6, "Seven":7, "Eight":8, "Nine":9, "Ten":10, "Queen":10, "Jack":10, "King":10}
+cards = {1:"Ace", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 6:"Six", 7:"Seven", 8:"Eight", 9:"Nine", 10:"Ten", 11:"Queen", 12:"Jack", 13:"King"}
+dealer_score = 0
+player_score = 0
+
+def draw_cards():
+    drawn_card = random.randint(1,13)
+    card = cards.get(drawn_card)
+    return card
+
+
+
